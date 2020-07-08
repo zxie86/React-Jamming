a82355ceac5c63b78adfe58b09d51405966bd800
Kelly <zxie86@uwo.ca>
2020-07-08 00:05:14 -0600

    stylesheet

---
+++ src/components/PlayList/PlayList.js
@@ -0,0 +1,14 @@
+import './PlayList.css';
+import TrackList from '../TrackList/TrackList';
+class PlayList extends React.Component {
+    render(){
+        return(
+            <div className="Playlist">
+            <input defaultValue ={'New Playlist'}/>
+            /* < TrackList /> */
+            <button className="Playlist-save">SAVE TO SPOTIFY</button>
+</div>
+        );
+    }
+}
+export default PlayList;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import './SearchResults.css'
+
+class SearchResults extends React.Component {
+    render(){
+        return(
+            <div className="SearchResults">
+            <h2>Results</h2>
+            < TrackList />
+            </div>
+        );
+    }
+}
+export default SearchResults;
